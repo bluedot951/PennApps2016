e16ef176bb456860e3013e3c076aaa7fe125f4fd
Joseph Zhong <joseph.zhong@hotmail.com>
2016-09-11 07:56:24 -0400

    wip

--- algos.py
+++ algos.py
@@ -103,11 +103,12 @@ def getData(pq, all_orders, ledger, new_order):
         else:
             SPQ.append(p)
 
+    print 'queues'
     print BPQ
     print SPQ
-    #
-    # BPQ.sort(key=lambda entry: (-entry.p, entry.ts))
-    # SPQ.sort(key=lambda entry: (entry.p, entry.ts))
+
+    BPQ.sort(key=lambda entry: (-entry.p, entry.ts))
+    SPQ.sort(key=lambda entry: (entry.p, entry.ts))
     execution(BPQ, SPQ, ledger)
 
     pqToRet = BPQ
